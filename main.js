40afa281e21c16b747f3ae794f4f2a8001e59ccf
mueez <mueezejaz12345678@gmail.com>
2025-11-29 15:21:22 +0500

    setting up project

---
+++ main.js
@@ -0,0 +1,12 @@
+import { app } from "electron";
+import createMainWindow from "./windows/mainWindow.js";
+app.whenReady().then(() => {
+  createMainWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ windows/mainWindow.js
@@ -0,0 +1,20 @@
+import { BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+  win.loadURL("http://localhost:5173");
+  return win;
+}
+
